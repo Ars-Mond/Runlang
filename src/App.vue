4a1ec650a9514d92c6832c2c3b02a06c97a07672
Ars_Mond <ars123.www@gmail.com>
2022-08-17 02:28:16 +0500

    Clear layout

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,19 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view />
+  <header>
+    <div class="main-menu">
+      <nav class="menu">
+        <router-link to="/">Home</router-link> |
+        <router-link to="/about">About</router-link>
+      </nav>
+      <router-view />
+    </div>
+  </header>
+  <main>
+
+  </main>
+  <footer>
+
+  </footer>
 </template>
 
 <style lang="less">
